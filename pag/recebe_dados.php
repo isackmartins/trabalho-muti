b57112bec70c7effb8fd3f9930560f4735c734c1
isackmartins <isackmartins79@gmail.com>
2024-09-21 16:04:34 -0300

    tentando arrumar

--- pag/recebe_dados.php
+++ pag/recebe_dados.php
@@ -1,9 +1,25 @@
-<?php
-if(isset($_POST["nome"]) or isset ($_POST["comentario"])){
-        $nome = $_POST["nome"];
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="../css/cadastro.css">
+</head>
+<body>
+
+<div class="coment";>
+      <div class ="comentario1";>
+        <?php
+        if(isset($_POST["comentario"])){
         $comentario = $_POST["comentario"];
-echo "<h2>Dados recebidos com sucesso!</h2>";
-echo "Usuário: $nome <br>";
-echo "Comentario: $comentario <br>";
-}
-?>
+        echo "<h2>Comentaro 1</h2>";
+        echo "Comentario: $comentario <br>";
+        }
+        ?>   
+         </div>
+</div>
+
+</body>
+</html>
